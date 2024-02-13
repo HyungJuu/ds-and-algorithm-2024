51d150c1fdb047eea83745bc57cb5f7b81a7733d
Geuna00 <rmsdk617@naver.com>
2024-02-13 17:30:02 +0900

    2일차 종료

--- day02/ds05_orderedList.py
+++ day02/ds05_orderedList.py
@@ -1,7 +1,6 @@
 # file : ds05_orderedList.py
 # desc : 다항식 선형리스트 표현과 계산 프로그램
 
-
 def printPoly(p_x):
     term = len(p_x) - 1 # 최고차항 숫자 = 배열 길이 -1
     polyStr = "P(x) = "
@@ -13,7 +12,6 @@ def printPoly(p_x):
             polyStr += "+"
         polyStr += str(coef) + "x^" + str(term) + " "
         term -= 1
-
     return polyStr
 
 def calcPoly (xVal, p_x):
@@ -24,7 +22,6 @@ def calcPoly (xVal, p_x):
         coef = p_x[i]
         retValue += coef * xVal ** term
         term -= 1
-    
     return retValue
 
 ## 전역 변수 선언    
@@ -39,4 +36,43 @@ if __name__ == '__main__':
     xValue = int(input('X 값 --> '))
 
     pxValue = calcPoly(xValue, px)
+    print(pxValue)
+
+
+## 특수 다항식의 선형리스트 표현과 계산 프로그램
+## 함수 선언
+def printPoly(t_x, p_x):
+    polyStr = "P(x) = "
+
+    for i in range(len(p_x)):
+        term = t_x[i] # 항 차수
+        coef = p_x[i] # 계수
+
+        if (coef >= 0):
+            polyStr += "+"
+        polyStr += str(coef) + "x^" + str(term) + " "
+    return polyStr
+
+def calcPoly (xVal, t_x, p_x):
+    retValue = 0
+
+    for i in range(len(px)):
+        term = t_x[i]
+        coef = p_x[i]
+        retValue += coef * xVal ** term
+    return retValue
+
+## 전역 변수 선언    
+tx = [300, 20, 0]
+px = [7, -4, 5]
+
+## 메인 코드
+if __name__ == '__main__':
+
+    pStr = printPoly(tx, px)
+    print(pStr)
+
+    xValue = int(input('X 값 --> '))
+
+    pxValue = calcPoly(xValue, tx, px)
     print(pxValue)
